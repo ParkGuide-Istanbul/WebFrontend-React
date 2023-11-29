5e1ed06a5eb63e519a29844bd1198ef8cbd0d947
ituitis-beydemir18 <alpbeydemir@hotmail.com>
2023-11-29 20:30:54 +0300

    logo changed,faviocan changed, backendendpoint add

--- parkguide-react/src/auth/AuthService.js
+++ parkguide-react/src/auth/AuthService.js
@@ -1,19 +1,20 @@
 const AuthService = {
     login: async (username, password) => {
-      const response = await fetch('YOUR_BACKEND_LOGIN_ENDPOINT', {
+      const response = await fetch('https://o11xc731wl.execute-api.eu-central-1.amazonaws.com/dev2/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ username, password}),
       });
+      debugger;
   
       if (!response.ok) {
         throw new Error('Login başarısız');
       }
   
       const data = await response.json();
-      return data.token; // JWT token'ı döndür
+      return data.message.token; // JWT token'ı döndür
     },
   
     // Buraya token ile ilgili diğer fonksiyonlar eklenebilir (örn. token'ı kaydetme)
