7c318b25294cb90cc8f24cbe1667b7ed518e9062
ituitis-beydemir18 <alpbeydemir@hotmail.com>
2023-11-30 09:07:58 +0300

    in Header logo, font size, visual changed

--- parkguide-react/src/components/Layout/Header.js
+++ parkguide-react/src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import UserDropdown from './UserDropdown';
 import './Header.css';
+import logo from '../../assets/logoaraba.png';
 
 const Header = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -8,6 +9,8 @@ const Header = () => {
 
   return (
     <header className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1>ParkGuide Istanbul</h1>
       <div className="user-section" onClick={() => setDropdownOpen(!dropdownOpen)}>
         <img src="/path/to/template-avatar.png" alt="User" className="user-image" />
         <span className="username">{username}</span>
